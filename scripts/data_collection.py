15b804b9f1ed82bffa55b9749f983793fb7d3f95
Pushkar Jajoria <pushkar.jajoria@gmail.com>
2020-05-08 19:37:28 +0200

    Latest code changes

--- scripts/data_collection.py
+++ scripts/data_collection.py
@@ -19,7 +19,7 @@ import thread
 import os
 from gazebo_msgs.msg import ModelState
 from gazebo_msgs.srv import SetModelState
-
+import cv2
 
 class ThymioController:
 
@@ -27,17 +27,14 @@ class ThymioController:
         """Initialization."""
 
         # Variable to store the current range of sensors 
-        # Initializing to 10 to avoid interference with collision detection  
+        # Initializing to 10 to avoid interference with collision detection
         self.left_sensor = 10
         self.center_left_sensor = 10
         self.center_sensor = 10
         self.center_right_sensor = 10
         self.right_sensor = 10
-        self.spawn_service = rospy.ServiceProxy('spawn', gazebo_msgs.srv.SpawnModel)
 
-
-        # Stopping threshold
-        self.threshold = 0.08
+        self.collision_tol = .07
 
         self.collided = False # Flag to stop the robot to avoid obstacles
 
@@ -49,8 +46,6 @@ class ThymioController:
 
         self.name = rospy.get_param('~robot_name')
 
-        self.distance_tolerance=.1
-
         # log robot name to console
         rospy.loginfo('Controlling %s' % self.name)
 
@@ -59,13 +54,6 @@ class ThymioController:
         self.name + '/cmd_vel',  # name of the topic
         Twist,  # message type
         queue_size=10  # queue size
-        )
-
-        # create pose subscriber
-        self.pose_subscriber = rospy.Subscriber(
-        self.name + '/odom',  # name of the topic
-        Odometry,  # message type
-        self.log_odometry  # function that hanldes incoming messages
         )
 
         ############################################################
@@ -105,8 +93,8 @@ class ThymioController:
         self.image_save_frequency = 1 # seconds
         self.image_count = 0
         self.image = []
-        self.dataset = []
-        self.collision_tol = .01
+        self.dataset_images = []
+        self.dataset_labels = []
 
 
         # tell ros to call stop when the program is terminated
@@ -125,92 +113,84 @@ class ThymioController:
     def log_sensor_right(self, data):
         """Subscriber callback for robot right sensor data"""
         self.right_sensor = data.range
-        if (data.range < self.threshold):
-            self.object_avoidance = True
-            self.STATE="HIT"
 
     def log_sensor_center_right(self, data):
         """Subscriber callback for robot right sensor data"""
         self.center_right_sensor = data.range
-        if (data.range < self.threshold):
-            self.object_avoidance = True
-            self.STATE="HIT"
 
     def log_sensor_center(self, data):
         """Subscriber callback for robot right sensor data"""
         self.center_sensor = data.range
-        if (data.range < self.threshold):
-            self.object_avoidance = True
-            self.STATE="HIT"
 
 
     def log_sensor_center_left(self, data):
         """Subscriber callback for robot right sensor data"""
         self.center_left_sensor = data.range
-        if (data.range < self.threshold):
-            self.object_avoidance = True
-            self.STATE="HIT"
 
     def log_sensor_left(self, data):
         """Subscriber callback for robot right sensor data"""
         self.left_sensor = data.range
-        if (data.range < self.threshold):
-            self.object_avoidance = True
-            self.STATE="HIT"
+
+
+    def create_dataset(self):
+        np.save("dataset/" + "dataset_images" + ".npy", self.dataset_images)
+        np.save("dataset/" + "dataset_labels" + ".npy", self.dataset_labels)
+        rospy.signal_shutdown("Data collection successfully completed!")
 
     def save_image(self):
-        image_stored = 0
-        while image_stored<=1000:
-            image_stored += 1           
-            im = self.image
-            if(len(im) == 0):
-                print("No Image Data Yet, Waiting...")
-                time.sleep(self.image_save_frequency)
-                continue
+        while True:
+            if(len(self.image)>0):
+                break;
+            time.sleep(self.image_save_frequency)
+
+        while self.image_count<=4000:
+            desired_size = (200,200)
+            current_image = cv2.resize(self.image, dsize=desired_size, interpolation=cv2.INTER_CUBIC)
+                        
             sensor_values = [self.left_sensor, 
                             self.center_left_sensor, 
                             self.center_sensor, 
                             self.center_right_sensor, 
                             self.right_sensor]
-            np_dictinary = {"image":im,"sensor":sensor_values}
+            self.dataset_images.append(current_image)
+            self.dataset_labels.append(sensor_values)
             self.image_count += 1
-            np.save("dataset/" + "/data_" + str(self.image_count) + ".npy", np_dictinary)
             time.sleep(self.image_save_frequency)
-        os.exit(1)
+        self.create_dataset()
 
 
     def process_image(self, data):
-        self.image = np.frombuffer(data.data, dtype=np.uint8).reshape(data.height, data.width, -1)
-        print(self.image.dtype)
-        print(self.image.shape)
-        print(type(self.image))
-    ## to respawn/teleport the robot if it crashes.
+        im = np.frombuffer(data.data, dtype=np.uint8).reshape(data.height, data.width, -1)
+        self.image = np.array(im, dtype=np.float32)
+
+        ## to respawn/teleport the robot if it crashes.
     def respawn(self,x,y,theta):
-    	vMsg = ModelState()
-    	vMsg.model_name = '/'+self.name
-    	
-    	vMsg.pose.position.x = float(x)
-    	vMsg.pose.position.y = float(y)
-    	vMsg.pose.position.z = 0
+        vMsg = ModelState()
+        vMsg.model_name = self.name
+        vMsg.model_name = vMsg.model_name[1:]
+        
+        vMsg.pose.position.x = float(x)
+        vMsg.pose.position.y = float(y)
+        vMsg.pose.position.z = 0
 
-    	vMsg.pose.orientation.x = 0
-    	vMsg.pose.orientation.y = 0
-    	vMsg.pose.orientation.z = float(theta)
-    	vMsg.pose.orientation.w = w
-    	rospy.wait_for_service('/gazebo/set_model_state')
+        vMsg.pose.orientation.x = 0
+        vMsg.pose.orientation.y = 0
+        vMsg.pose.orientation.z = float(theta)
+        vMsg.pose.orientation.w = 1
+        rospy.wait_for_service('/gazebo/set_model_state')
 
-    	try:
-    		vState = rospy.ServiceProxy('/gazebo/set_model_state', SetModelState)
-    		return vState(vMsg)
-    	except rospy.ServiceException, e:
-    		print(e)
+        try:
+            vState = rospy.ServiceProxy('/gazebo/set_model_state', SetModelState)
+            return vState(vMsg)
+        except rospy.ServiceException, e:
+            print(e)
+
 
     def get_control(self):
-        # to put a higher limit on total velocity
         angular_z = 0
         return Twist(
         linear=Vector3(
-        .25,  # moves forward .2 m/s
+        .25,  # moves forward .25 m/s
         0,
         .0,
         ),
@@ -222,42 +202,38 @@ class ThymioController:
         )
 
     def run(self):
- #       self.create_directory()
-       thread.start_new_thread(self.save_image, ())
-
-        """Controls the Thymio."""
+        thread.start_new_thread(self.save_image, ())
         while not rospy.is_shutdown():
-        # Set a subscriber to all the proximity sensors
-        # Keep Move forward
-        # Stop moving forward if any sensor below threshold
-        # Take a little step back
-        # Rotate to a random angle behind the obstacle
-        # keep doing this as part of the exploration
-        sensor_values = np.array([self.left_sensor, 
-                        self.center_left_sensor, 
-                        self.center_sensor, 
-                        self.center_right_sensor, 
-                        self.right_sensor])
-        if self.collided==True:
-        	vX,vY = np.random.uniform(,0,1,2)*15
-        	vTheta = np.random.uniform(0,1)*90
-        	self.respawn(vX,vY,vTheta)
-        	self.collided=False
+            if self.collided==True:
+                vX,vY = np.random.uniform(-1,1,2)*15
+                vTheta = np.random.uniform(0,1)*90
+                self.respawn(0,0,0)
+                self.collided=False
 
 
             while not self.collided:
                 # decide control action
-                velocity = self.get_control(randomness = False)
+                velocity = self.get_control()
                 # publish velocity message
                 self.velocity_publisher.publish(velocity)
-                if any(abs(sensor_values))<self.collision_tol:
-                	self.collided=True
+                
+                vsensor = np.array([self.left_sensor, 
+                            self.center_left_sensor, 
+                            self.center_sensor, 
+                            self.center_right_sensor, 
+                            self.right_sensor])
+
+                if min(vsensor)<self.collision_tol:
+                    print("*"*25)
+                    print("Collision!!!!")
+                    self.collided=True
 
                 # sleep until next step
             self.rate.sleep()
 
  #           self.collided = False # Set the flag back
         return
+
 
     def stop(self):
         """Stops the robot."""
@@ -273,7 +249,6 @@ if __name__ == '__main__':
     controller = ThymioController()
 
     try:
-        time.sleep(15) # Added a short sleep for time to open the simulation
         controller.run()
     except rospy.ROSInterruptException as e:
         pass
